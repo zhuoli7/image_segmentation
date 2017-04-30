d66efbd8647eff9ecdf9bb85ef5cacd33933d8c0
ShawnHan <shawnhan1029@gmail.com>
2017-04-30 15:15:23 -0400

    UI v2.0

--- nfis_UI.py
+++ nfis_UI.py
@@ -3,6 +3,9 @@ from PIL import ImageTk, Image
 from tkinter.filedialog import askopenfilename
 from pathlib import Path
 
+clicked_list = []
+resize_factor = 0
+
 class input_item(Frame):
     def __init__(self, title_str, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
@@ -22,34 +25,40 @@ resize_factor = 0
 def click_handler(event):# this is the handler of the mouse clicking
     print("Mouse position: (%s %s)" % (event.x, event.y))
     global origin_image_label
+    clicked_list.append([event.x, event.y])
     origin_image_label.create_oval(event.x - 1, event.y - 1, event.x + 1, event.y + 1, width = 2, fill = 'red', outline = 'red')
+    return
+
+def update_result():# this is the handler of the start processing button
     global sample_rate_input
     global penalty_input
+    global img2
+    global clicked_list
+
+    path2 = output_image_name.get_text()   # change this line to the name of output image
     sample_rate = float(sample_rate_input.get_text())#sampling rate
     penalty = float(penalty_input.get_text())   #penalty set
     #the default value is 4 for sample_rate, 0.02 for penalty.
-
     #----------------put your code here------------------#
 
 
     #----------------------------------------------------#
-    return
-
-def update_result():
-    global img2
-    global path2
     img2 = Image.open(path2)
     img2 = ImageTk.PhotoImage(img2.resize((int(img2.width/resize_factor), int(img2.height/resize_factor))))
     result_image_label.configure(image = img2)
     return
 
 def open_file():
-    path = askopenfilename()
+    global img
+    global resize_factor
+    global clicked_list
+    
+    clicked_list = []
+    path = input_image_name.get_text()
     img = Image.open(path)
-    resize_factor = img.height / 300 if (img.height / 300) > (img.width / 400) else img.width / 400
+    resize_factor = img.height / 300 if (img.height / 300) > (img.width / 500) else img.width / 500
     img = ImageTk.PhotoImage(img.resize((int(img.width/resize_factor), int(img.height/resize_factor))))
-    origin_image_label.configure(image = img)
-
+    origin_image_label.create_image(0, 0, anchor=NW, image =img)
 
 
 foreground_list = []
@@ -57,26 +66,31 @@ foreground_list = []
 #path = Path(path)
 window = Tk()
 window.title('nfis')
-path = "example.jpg"  # change this line to the name of input image
-path = "result.jpg"   # change this line to the name of output image
-img = Image.open(path)
-resize_factor = img.height / 300 if (img.height / 300) > (img.width / 400) else img.width / 400
-img = ImageTk.PhotoImage(img.resize((int(img.width/resize_factor), int(img.height/resize_factor))))
+
 image_part = Frame(window)
-panel_part = LabelFrame(window, text="panel", width = 800, height = 70)
+panel_part = LabelFrame(window, text="panel", width = 1000, height = 70)
 panel_part.pack_propagate(0)
 image_part.pack(side = "top")
 panel_part.pack(side = "bottom")
 
-origin_image_frame = LabelFrame(image_part, text="origin image", width = 400, height = 300)
-result_image_frame = LabelFrame(image_part, text="result image", width = 400, height = 300)
+input_image_name = input_item('input', panel_part)
+input_image_name.insert('example.jpg')
+input_image_name.pack(side = "left")
+open_button = Button(panel_part, text="open", command = open_file)
+open_button.pack(side = "left", padx=10)
+
+output_image_name = input_item('output', panel_part)
+output_image_name.insert('result.jpg')
+output_image_name.pack(side = "left", padx=10)
+
+origin_image_frame = LabelFrame(image_part, text="origin image", width = 500, height = 300)
+result_image_frame = LabelFrame(image_part, text="result image", width = 500, height = 300)
 origin_image_frame.pack(expand = "yes", side = "left")
 result_image_frame.pack(expand = "yes", side = "right")
 origin_image_frame.pack_propagate(0)
 result_image_frame.pack_propagate(0)
-origin_image_label = Canvas(origin_image_frame)
+origin_image_label = Canvas(origin_image_frame, width = 500, height = 300)
 origin_image_label.pack_propagate(0)
-origin_image_label.create_image(0, 0, anchor=NW, image = img)
 result_image_label = Label(result_image_frame)
 origin_image_label.bind('<Button>', click_handler)
 result_image_label.bind('<Button>', click_handler)
